1516254bd87315355d37f82c487a3b7933bd4d22
cnadrag <nadrag.carmen@yahoo.ro>
2018-05-06 14:18:44 +0300

    Update filtru_cuvinte.c

--- filtru_cuvinte.c
+++ filtru_cuvinte.c
@@ -2,30 +2,40 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define N 5120
+
 int main()
 {
-    int n,i,j;
-    char s[5122],nep[5122];
+    int i,j,n;
+    char s[N+1],nep[N+1];
     char *p;
     
     
-    fgets(s, sizeof s, stdin);
-    s[strlen(s)-1] = '\0';
+    fgets(s, N, stdin);
+    s[N]='\0';
+    if(s[strlen(s)-1]=='\n')
+        {
+            s[strlen(s)-1] = '\0';
+        }
     scanf("%d\n", &n);
     for(i=0;i<n;i++)
         {
-            scanf("%s ", nep);
+            scanf("%s", nep);
             p = strstr(s, nep);
             while(p!=NULL)
                 {
                     for(j=0;j<strlen(nep);j++)
-                         p[j] = '*';
-                         p = strstr(p,nep);
-                       
-                    
+                       {
+                           p[j] = '*';
+                       }
+                    p = strstr(p,nep);
                 }            
             
         }
-    printf("%s\n", s);    
-    
+    //problema: cand se citeste sirul, se citeste si \n introdus la sfarsitul lui
+    //printf("%s\n", s);    
+    // ca sa adaug un \n la printf, tre sa folosesc if-ul, pt ca altfel raman cu 2*\n
+    puts(s);
+    // adauga el inca un \n, deci tre sa folosesc if-ul acela...pt ca altfel raman cu 2*\n
+
 }
