1dd65850677e3f0462da049695c6e3260c29ae67
cnadrag <nadrag.carmen@yahoo.ro>
2018-06-01 19:17:21 +0300

    Update subsir_nr_pozitive.c

--- subsir_nr_pozitive.c
+++ subsir_nr_pozitive.c
@@ -1,54 +1,50 @@
 #include<stdio.h>
-#include<math.h>
 
 int main()
 {
-    int n,i=0, index,local_index,len=0,local_len;
-    double x, local_sum, max_sum;
+    int i=0,n,index,lung, lung_max=0,ind;
+    float x,suma,suma_max=0;
     scanf("%d\n", &n);
-    
     while(i<n)
         {
-            local_index = i;
-            local_sum = 0;
-            local_len = 0;
+            index = i;
+            suma = 0;
+            lung = 0;
             while(i<n)
                 {
-                    scanf("%lf\n", &x);
+                    scanf("%f ", &x);
                     i++;
                     if(x>0)
                         {
-                            local_sum += x;
-                            local_len++;
+                            suma+=x;
+                            lung += 1;
+                            
                         }
-                    else 
-                        {   
+                    else
+                        {
                             break;
+                            
                         }
-                    
-                }   
+                }
+                
+                    if((lung>lung_max) ||((lung==lung_max) && (suma>suma_max))) 
+                        {
+                            ind = index; //tb actualizat si index, altfel o sa am suma max si lung max, DAR MEREU AFISEAZA ULTIMUL INDEX!!
+                            suma_max = suma;
+                            lung_max = lung;
+                         
+                        }
+                
+        }
     
-            if((len < local_len) ||   (   (len == local_len) && (local_sum > max_sum)  )   )
-                    {
-                        index = local_index;
-                        len = local_len;
-                        max_sum = local_sum;
-                        
-                    }
-        }                
-    if(len==0)
+    if(lung_max==0)
         {
-            printf("-1 0");
+           printf("-1 0");
         }
-    else
+    else 
         {
-            printf("%d %d\n", index, len);
+            
+             printf("%d %d\n", ind, lung_max);
         }
-        
-        
+    
 }
-
-
-
-
-
