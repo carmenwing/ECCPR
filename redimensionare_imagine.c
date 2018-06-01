b462f31a64dcb944dd29fbcc418b8b25588409ba
cnadrag <nadrag.carmen@yahoo.ro>
2018-06-01 21:52:25 +0300

    Update redimensionare_imagine.c

--- redimensionare_imagine.c
+++ redimensionare_imagine.c
@@ -1,65 +1,68 @@
 #include<stdio.h>
 
-int m,s;//trebuie sa fie globale
-struct triplet {
-                    int v[3];
-                };
+ int m,s,n;
+ struct tip {
+                        int r;
+                        int g;
+                        int b;
+    };
+struct tip elem[21][21],max; 
 
-struct triplet a[21][21];
-
-triplet alocare_maxim (int x, int y)
-    {
-        int i,j;
-        triplet max;
-        max.v[0] = a[x][y].v[0];
-        max.v[1] = a[x][y].v[1];
-        max.v[2] = a[x][y].v[2];
-        
-        for(i=x;i<x+s;i++)
-            {
-                for(j=y;j<y+s;j++)
-                    {
-                        if(a[i][j].v[0]>max.v[0]) 
-                                {
-                                    max.v[0] = a[i][j].v[0];
-                                }
-                        if(a[i][j].v[1]>max.v[1]) 
-                                {
-                                    max.v[1] = a[i][j].v[1];
-                                }
-                        if(a[i][j].v[2]>max.v[2]) 
-                                {
-                                    max.v[2] = a[i][j].v[2];
-                                }
-                    }
-            }
-        return max;    
-    }
-
-
+tip maxim(int x, int y)
+{
+    int i,j;
+    max.r = elem[x][y].r;
+    max.g = elem[x][y].g;
+    max.b = elem[x][y].b;
+    for(i=x;i<x+s;i++)
+        {
+            for(j=y;j<y+s;j++)
+                {
+                    if(max.r<elem[i][j].r)
+                        {
+                            max.r = elem[i][j].r;
+                        }
+                    if(max.g<elem[i][j].g)
+                        {
+                            max.g = elem[i][j].g;
+                        }
+                    if(max.b<elem[i][j].b)
+                        {
+                            max.b = elem[i][j].b;
+                        }    
+                }
+        }
+    return max;    
+}
 
 int main()
 {
+   
     int i,j;
-    triplet maxim;
-    
     scanf("%d\n", &m);
     scanf("%d\n", &s);
+    n = m/s;
+    struct tip v[n][n],rez;
     
     for(i=0;i<m;i++)
         {
             for(j=0;j<m;j++)
-            {
-                scanf("%d %d %d\n", &a[i][j].v[0], &a[i][j].v[1], &a[i][j].v[2]);
-            }
+                {
+                    scanf("%d %d %d\n", &elem[i][j].r, &elem[i][j].g, &elem[i][j].b );
+                }
         }
-        
-    for(i=0;i<m;i+=s)        
+    
+    for(i=0;i<m;i+=s)
         {
             for(j=0;j<m;j+=s)
                 {
-                    maxim = alocare_maxim(i,j);
-                    printf("%d %d %d\n", maxim.v[0], maxim.v[1], maxim.v[2]);
+                    rez = maxim(i,j);
+                    printf("%d %d %d\n", rez.r, rez.g, rez.b);
                 }
         }
+            
+    //indicii i si j trebuie declarat local, altfel apare conflict!!!!        
+            
+            
+            
 }
