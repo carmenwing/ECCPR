06fdc807652d98369359608c8ec12ace05f66e93
cnadrag <nadrag.carmen@yahoo.ro>
2018-06-02 00:56:03 +0300

    Update codare.c

--- codare.c
+++ codare.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
+
 int main()
 {
-    int n, i, v[7],j,suma, a,max=0;
-    scanf("%d ", &n);
-    while(n>0)
-    
-    {     
+    int n,i,j,max=0, sum=0;
+    int v[7];
+    scanf("%d\n", &n);
+    while(n--)
+    {
+        scanf("%d %d %d %d %d %d %d\n", &v[1],&v[0],&v[3],&v[2],&v[5],&v[4],&v[6]);
+        sum = v[0];
+        for(j=1;j<=6;j++)
+            {
+                v[j] = (v[j]%2);
+                sum = sum +  v[j];
+            }
+        if(max<=sum)
+            {
+                max = sum;
+            }
            
-            scanf("%d %d %d %d %d %d %d\n", &v[1],&v[0],&v[3],&v[2],&v[5],&v[4],&v[6]);
-            suma=v[0];        
-            for (i=1;i<=6;i++)
-                        {
-                           suma = suma+(v[i]%2);
-                        }
-                        
-            if(suma>=max)
-                max = suma;
-            n--;
-    } 
+    }
     
-       printf("%d\n", max); 
+    printf("%d\n", max);
+    
 }
+//ATENTIE IN FOR-URI PANA LA CINE MERG!!!!! DACA E < SAU <= SI DACA MERG PANA LA VARIABILA CARE TREBUIE, NU PANA LA NR DE LINII
+//SAU ALTCEVA!!
