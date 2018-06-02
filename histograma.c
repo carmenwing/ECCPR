13ae169412f713a180d80f03d4bc2832f8f3840e
cnadrag <nadrag.carmen@yahoo.ro>
2018-06-02 16:45:47 +0300

    Create histograma.c

---
+++ histograma.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+int main()
+
+
+{
+    #define N 1000
+    char s[N+2];
+    char *p;
+    int i,j, ok=0, nr=0;
+    struct tip {
+                    char cuv[N+1];
+                    int ap;
+    };
+    
+    struct tip lista[N], temp;
+    fgets(s, sizeof(s), stdin);
+    s[strlen(s)-1] = '\0';
+    
+     for(i=0;i<strlen(s);i++)
+        {
+            s[i] = tolower(s[i]);
+        }
+    p = strtok(s," ");
+    while(p)
+        {
+            ok = 0;
+            for(i=0;i<nr;i++)   
+                {
+                    if(strcmp(lista[i].cuv,p)==0)
+                        {
+                            lista[i].ap+=1;
+                            ok=1;
+                            break;
+                        }
+                }//AICI SE INCHEIE FOR-UL!!!            
+            if(ok==0) 
+            {
+                strcpy(lista[nr].cuv,p);
+                lista[nr].ap = 1;
+                nr++;                   
+            }
+        
+            p = strtok(NULL, " ");    
+            
+        }
+    for(i=0;i<nr-1;i++)
+        {
+            for(j=i+1;j<nr;j++)
+                {
+                    if((lista[j].ap>lista[i].ap) || ((lista[j].ap==lista[i].ap)&&(strcmp(lista[i].cuv,lista[j].cuv)>0)))
+                        {
+                            temp = lista[i];
+                            lista[i] = lista[j];
+                            lista[j] = temp;
+                        }
+                }
+        }
+   
+   for(i=0;i<nr;i++)
+    {
+        printf("%s %d\n", lista[i].cuv, lista[i].ap);
+    }
+    //NU A MERS CU S[1001], TREBUIE S[1002]!!!!
+    
+}
