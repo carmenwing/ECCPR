c4412731107344a207d20d826e2b707bf7ba7f8c
cnadrag <nadrag.carmen@yahoo.ro>
2018-06-03 18:19:09 +0300

    Update nave_cosmice_gresit.c

--- nave_cosmice_gresit.c
+++ nave_cosmice_gresit.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    int n,i,j, index;
+    int n, i, j, index;
     double d_x, d_y, d_z, min, sum=0;
     struct tip {
         double x,y,z,d;
@@ -20,48 +20,40 @@ int main()
         }
     scanf("%lf %lf %lf\n", &pozitie.x, &pozitie.y, &pozitie.z);
     
-   while(n--)
-        {    
-             
-             for(i=0;i<n;i++)
+    while(n--)
+    {    
+        int minim_initializat = 0;
+
+        for(j=0;j<n;j++)
+        {
+            if( coord[j].vizitat == 0)
+            {
+                d_x = pow((pozitie.x-coord[j].x),2);
+                d_y = pow((pozitie.y-coord[j].y),2);
+                d_z = pow((pozitie.z-coord[j].z),2);
+                coord[j].d = sqrt(d_x + d_y + d_z);
+
+                if (minim_initializat == 0)
                 {
-                           if( coord[i].vizitat == 0)
-                            {
-                                    d_x = pow((pozitie.x-coord[i].x),2);
-                                    d_y = pow((pozitie.y-coord[i].y),2);
-                                    d_z = pow((pozitie.z-coord[i].z),2);
-                                    coord[i].d = sqrt(d_x + d_y + d_z);
-                                    min = coord[i].d;
-                                    index=i;
-                                    break;
-                            }
-                            
-                            
-               }
-             for(j=i+1;j<n;j++)
-                            {
-                                if( coord[j].vizitat == 0)
-                                        {
-                                                d_x = pow((pozitie.x-coord[j].x),2);
-                                                d_y = pow((pozitie.y-coord[j].y),2);
-                                                d_z = pow((pozitie.z-coord[j].z),2);
-                                                coord[j].d = sqrt(d_x + d_y + d_z);
-                                                if(coord[j].d<min)
-                                                    {
-                                                        min = coord[j].d;
-                                                        index = j;
-                                                    }
-                                        }
-                            }
-                        
-           
-            sum = sum + min;
-            coord[index].vizitat = 1;
-            pozitie.x = coord[index].x;
-            pozitie.y = coord[index].y;
-            pozitie.z = coord[index].z;
+                    min = coord[j].d;
+                    minim_initializat = 1;
+                    index = j;
+                } else {
+                    if(coord[j].d<min)
+                    {
+                        min = coord[j].d;
+                        index = j;
+                    }
+                }
+            }
         }
-    
+        
+        sum = sum + min;
+        coord[index].vizitat = 1;
+        pozitie.x = coord[index].x;
+        pozitie.y = coord[index].y;
+        pozitie.z = coord[index].z;
+    }
     
     printf("%.2lf\n", sum);
 }
