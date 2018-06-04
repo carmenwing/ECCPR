fc592b712363aa29145b7f2728c4bdf8803eb371
cnadrag <nadrag.carmen@yahoo.ro>
2018-06-05 00:05:14 +0300

    Update maximele_semnalului.c

--- maximele_semnalului.c
+++ maximele_semnalului.c
@@ -1,62 +1,39 @@
 #include<stdio.h>
 
 int main()
-
-
 {
-    int m,i,c=0;
-    scanf("%d\n",&m);
-    float v[m], media=0, suma=0;
-            
-    for(i=0;i<m;i++)
-          scanf("%f\n",&v[i]);
-          
-          
-    if ((v[0]>v[1]) && (v[0]>0))   
-        
-            {    
-                suma = suma+v[0];
-                c++;
-                 
-            } 
-            
-      
-        
-   if ((v[m-1]>v[m-2]) && (v[m-1]>0)) 
-        
-                    {   
-                        suma = suma+v[m-1];
-                        c++;
-                        
-                    }    
-            
+    int n, i, val_max = 0, nr_mai_mari = 0;
+    float v[200], suma = 0, media;
     
-    
-     for(i=1;i<(m-1);i++)
-     
+    scanf("%d\n", &n);
+    for( i = 0; i < n; i++)
         {
-            
-            if((v[i]>v[i-1]) && (v[i]>v[i+1]))
-                {
-                    suma = suma+v[i];
-                    c++;
-                }
-            
-            
+            scanf("%f\n", &v[i]);
         }
+    if ( (v[0] > 0) && (v[0] > v[1]) )   
+        {
+            nr_mai_mari++;
+            suma = suma + v[0];
+        }    
         
-    media = suma/c;
-    c=0;
-    
-    for(i=0;i<m;i++)
-        if(v[i]>media)
-            c++;
-    
-    printf("%d\n",c);
-  
-    
-    
-    
-    
-    
+    if  ( (v[n-1] > 0) && (v[n-1] > v[n-2]))
+        {
+            nr_mai_mari++;
+            suma = suma + v[n-1];
+        } 
+     for( i = 1; i < n-1; i++)
+        {
+            if ((v[i] > v[i-1]) && (v[i] > v[i+1]))
+                 {
+                    nr_mai_mari++;
+                    suma = suma + v[i];
+                 } 
+        }
+    media =    (float) suma / nr_mai_mari; 
+    for( i = 0; i < n; i++)
+        {
+            if(v[i]  > media)
+                val_max++;
+        }
+    printf("%d\n", val_max);
 }
