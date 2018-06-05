0883cc094a2a07350112cb02fbcc84fe54ef2bc1
cnadrag <nadrag.carmen@yahoo.ro>
2018-06-05 04:14:37 +0300

    Update numere_inmatriculare_gresit.c

--- numere_inmatriculare_gresit.c
+++ numere_inmatriculare_gresit.c
@@ -4,49 +4,57 @@
 
 int main () {
    char s1[4], s2[4], s3[4];
-   int i,j, ok1 = 0, ok2 = 0, ok3 = 0, c = 0;
+   int i,j, ok=0, c = 0;
    char jud[42][3]={{"AB"},{"AR"},{"AG"},{"B"},{"BC"},{"BH"},{"BN"},{"BT"},{"BV"},{"BR"},{"BZ"},{"CS"},{"CL"},{"CJ"},{"CT"},{"CV"},{"DB"},{"DJ"},{"GL"},
     {"GR"},{"GJ"},{"HR"},{"HD"},{"IL"},{"IS"},{"IF"},{"MM"},{"MH"},{"MS"},{"NT"},{"OT"},{"PH"},{"SM"},{"SJ"},{"SB"},{"SV"},{"TR"},{"TM"},{"TL"},{"VS"},{"VL"},{"VN"}};
    
    while(!feof(stdin))
     {
-        ok1 = 0;
-        ok2 = 0;
-        ok3 = 0;
+        ok = 0;
         scanf("%s %s %s\n", s1, s2, s3);
         //VERIFIC JUDET
-        for( i  = 0; i < 43; i++)
+        for( i  = 0; i < 42; i++)
             {
                 if(strcmp( s1, jud[i]) == 0) 
                     {
-                        ok1 = 1;  
+                        ok = 1;  
+                        break;
                     }
             }
         //VERIFIC CIFRELE    
-        c = 0;    
-        if((strlen(s2) ==2) || (strlen(s2) == 3))
+        if((strlen(s2) < 2) || (strlen(s2) > 3)) ok = 0; !!!!!!! sau, nu SI!!!
+        if (ok)
             {
                 for( i = 0; i< strlen(s2); i++)
                     {
-                        if( isdigit(s2[i]) !=0 ) c++;
+                        if( isdigit(s2[i]) == 0 )
+                            {
+                                ok = 0;
+                                break;
+                            }
+                            
                     }
-                if(c == strlen(s2))
-                    ok2 = 1;
+                
             }
-        c = 0;    
+          
         //VERIFIC LITERELE
-         if(strlen(s3) ==3)
+         if(strlen(s3) !=3) ok = 0;
+         if(ok)
             {
                 for (i = 0; i< 3; i++)
                     { 
-                        if( isupper(s3[i]) != 0) c++;
+                        if( isupper(s3[i]) == 0) 
+                            {
+                                ok = 0;
+                                break;
+                            }
+                            
                     }
-                if(c == strlen(s2))
-                    ok3 = 1;    
+                  
             }
             
             
-        if( (ok1==1) && (ok2==1) && (ok3==1)) printf("%s %s %s\n", s1, s2, s3);
+        if(ok) printf("%s %s %s\n", s1, s2, s3);
         
         
     }
