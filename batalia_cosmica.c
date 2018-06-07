4d54f0b76c71ddab11b30c9a1d4d45b0a0680074
cnadrag <nadrag.carmen@yahoo.ro>
2018-06-08 02:06:57 +0300

    Update batalia_cosmica.c

--- batalia_cosmica.c
+++ batalia_cosmica.c
@@ -1,41 +1,39 @@
 #include<stdio.h>
 int main()
+
+
 {
-    int n, i,ps,pf,a,b,c=0;
-    scanf("%d %d\n", &ps, &pf);
-    scanf("%d\n", &n);
-    a=ps;
-    b=pf;
-    struct valori {
-        int ps;
-        int pf;
-    };
-    
-    struct valori nava[n];
-    
-    for(i=0;i<n;i++)
+   int n, i, j,scut, fazer, ps, pf, c = 0;
+   scanf("%d %d\n", &ps, &pf);
+   scanf("%d\n", &n);
+   scut = ps;
+   fazer = pf;
+   struct tip {
+                    int ps;
+                    int pf;
+                };
+   struct tip nava[n];      
+   for ( i = 0; i < n; i++)
     {
-        ps=a;
-        pf=b;
         scanf("%d %d\n", &nava[i].ps, &nava[i].pf);
-        
-        while(ps>=0 && nava[i].ps>=0)
-            {
-                ps = ps - nava[i].pf;
-                
-                if(ps>=0)
-                {
-                    nava[i].ps = nava[i].ps - pf;
-                    if(nava[i].ps<0)
-                        c++;
-                        
-                }
-                       
-                
-            }
-        
-        
-        
     }
-    printf("%d ", c);
+    
+    for ( i = 0; i < n; i++)
+        {
+            ps = scut;
+            pf = fazer;
+            while(nava[i].ps >= 0) //sau sterg else-ul dar conditia devine while((nava[i].ps >= 0) && (ps >= 0))
+            {                           //sa stie ce sa faca daca nava mea moare!! 
+                ps = ps - nava[i].pf;
+                if (ps >= 0)
+                    nava[i].ps = nava[i].ps - pf;
+                else break;    
+            }
+            if ((nava[i].ps < 0) && (ps>=0)) c++;
+        }
+    printf("%d\n", c);
+    
+    
+    
+    
 }
