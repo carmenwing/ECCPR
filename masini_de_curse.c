a5ca35664826d6e62fd3595d3d8a8891c5a00d4a
cnadrag <nadrag.carmen@yahoo.ro>
2018-06-08 13:45:51 +0300

    Update masini_de_curse.c

--- masini_de_curse.c
+++ masini_de_curse.c
@@ -1,27 +1,35 @@
+#include<stdlib.h>
 #include<stdio.h>
+int n;
+
 
 int main()
 {
-    int n,k,i, p=0,v=0,min=0;
-    scanf("%d %d", &n, &k);
-    int t[k];
     
-    for(i=0;i<k;i++)
-        {  scanf("%d %d\n", &p, &v);
-           t[i]  = (n-p)/v;
+    
+    int  i, k, min, min_initializat=0;
+    scanf("%d %d\n", &n, &k);
+    int p, v, timp[n];
+    
+    for(i=0;i<=n;i++)
+        {
+            scanf("%d %d\n", &p, &v);
+            timp[i] = (n-p)/v;
+            if (min_initializat == 0)
+                {
+                    min = timp[i];
+                    min_initializat=1;
+                }
+            if(min>timp[i])
+                {
+                    min = timp[i];
+                }    
         }
-        
-    min = t[0];
-    for(i=0;i<k;i++)
-        {  if(t[i]<min)
-           min = t[i];
-        }       
-    for(i=0;i<k;i++)       
-        {  if (t[i]>min)
-           t[i] = -1;  
-        }            
-    for(i=0;i<k;i++)   
-        {       if(t[i]!= -1)
-                printf("%d ", i+1);
+      for(i=0;i<=n;i++)
+        {
+            if(timp[i]==min)
+                {
+                    printf("%d ", i+1);
+                }
         }
 }
